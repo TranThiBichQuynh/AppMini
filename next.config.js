2e369da51f0591f14b1d08bcd950297dc8330bf3
Tran Thi Bich Quynh <quynhttb@tks.asia>
2023-11-30 17:37:33 +0700

    up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// const nextConfig = {}
+//
+// module.exports = nextConfig
+module.exports = {
+    images: {
+        domains: ['images.microcms-assets.io', 'profile.line-scdn.net'],
+    },
+};
