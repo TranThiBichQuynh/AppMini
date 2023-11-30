5df29fbcd19a856d56109e24fefd084693ef41a3
Tran Thi Bich Quynh <quynhttb@tks.asia>
2023-11-30 14:24:43 +0700

    up

--- next.config.js
+++ next.config.js
@@ -2,3 +2,13 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+const nextjsDistDir = join("src", require("./src/next.config.js").distDir);
+const nextjsServer = next({
+    dev: isDev,
+    conf: {
+        distDir: nextjsDistDir,
+        images: {
+            domains: ['firebasestorage.googleapis.com'],
+        }
+    }
+});
